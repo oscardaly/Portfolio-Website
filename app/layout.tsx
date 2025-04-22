9dcce349a22bba5f936e54b635d0f93f04e45ed7
Oscar Daly <oscar.daly@lacework.net>
2025-04-22 01:13:46 +0100

    Add shadcn and tailwind - WIP, clean up unused files and config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'OJD - Portfolio',
+  title: 'Oscar Daly - Portfolio',
   description: 'This is a website to store and show my side projects and experience.',
 };
 
